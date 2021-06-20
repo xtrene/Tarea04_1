6102d2b7d51cbecdd9728ba2dcd5680a6ca0b555
Carlos <carlos@xtrene.com>
2021-06-20 23:59:10 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tarea04_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 7;
+            int c= 23;
+            int suma = a + b + c;
+
+            Console.WriteLine("La suma de a({0}) + b({1}) + c({2}) es: {3}",a,b,c, suma);
+
+
+            Console.ReadKey();
+        }
+    }
+}
